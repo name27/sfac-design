76581a71304e4d4c9fc48dfc39d132023c6aa6cc
judy <kje010207@gmail.com>
2023-06-28 13:25:38 +0900

    속성값 수정

--- lib/widget/keyword.dart
+++ lib/widget/keyword.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets/util/sfac_color.dart';
+import 'package:widgets/util/sfac_text_style.dart';
 
 class Keyword extends StatelessWidget {
   const Keyword(
@@ -7,15 +9,43 @@ class Keyword extends StatelessWidget {
       this.borderRadius = 10,
       this.spacing = 5,
       this.backgroundColor,
-      this.outlineColor});
+      this.outlineColor,
+      this.textStyle,
+      this.outlineWidth  = 1.0});
   final List<Widget?> keyword;
+  final TextStyle? textStyle;
   final double spacing;
   final double borderRadius;
   final Color? backgroundColor;
   final Color? outlineColor;
+  final double outlineWidth;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    List<Widget>? keywordText;
+    TextStyle? keywordStyle;
+    keywordStyle =
+        textStyle ?? SfacTextStyle.b5R12(color: SfacColor.grayScale60);
+    keywordText = keyword
+        .map((e) => AnimatedDefaultTextStyle(
+              style: keywordStyle!,
+              duration: kThemeChangeDuration,
+              child: e!,
+            ))
+        .toList();
+    return Wrap(
+        children: keywordText
+            .map((e) => Chip(
+                  label: e,
+                  backgroundColor: backgroundColor ?? SfacColor.grayScale5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(borderRadius),
+                    side: BorderSide(
+                      color: outlineColor ?? SfacColor.grayScale5,
+                      width: outlineWidth,
+                    ),
+                  ),
+                ))
+            .toList());
   }
 }
