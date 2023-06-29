4f15f45f588487f7d506d16164836628fedda8d5
marco <tlstmdgh8620@gmail.com>
2023-06-29 17:32:44 +0900

    icon

---
+++ lib/util/sfac_icon.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:widgets/widget/icon.dart';
+
+class SfacIcon{
+  //뒤로가기 아이콘
+  static Widget backarrow({double width = 24.0,double height = 24.0}) => SfacIconWidget('backarrow', width: width,height: height);
+
+  //레고 아이콘
+  static Widget block({double width = 24.0,double height = 24.0}) => SfacIconWidget('block', width: width,height: height);
+
+  //
+  static Widget coberbox({double width = 24.0,double height = 24.0}) => SfacIconWidget('coberbox', width: width,height: height);
+
+  //다트아이콘
+  static Widget dart({double width = 24.0,double height = 24.0}) => SfacIconWidget('dart', width: width,height: height);
+
+  //불 아이콘
+  static Widget fire({double width = 24.0,double height = 24.0}) =>SfacIconWidget('fire', width: width,height: height);
+
+  //게임패드 아이콘
+  static Widget gamepad({double width = 24.0,double height = 24.0}) => SfacIconWidget('gamepad', width: width,height: height);
+
+  //그래프 아이콘
+  static Widget graph({double width = 24.0,double height = 24.0}) => SfacIconWidget('graph', width: width,height: height);
+
+  //
+  static Widget group427321414({double width = 24.0,double height = 24.0}) => SfacIconWidget('group427321414', width: width,height: height);
+
+  //
+  static Widget group427321415({double width = 24.0,double height = 24.0}) => SfacIconWidget('roup427321415', width: width,height: height);
+  
+  //천사링 모양? 아이콘
+  static Widget halo({double width = 24.0,double height = 24.0}) => SfacIconWidget('halo', width: width,height: height);
+  
+  //손 흔드는 아이콘
+  static Widget hand({double width = 24.0,double height = 24.0}) =>SfacIconWidget('hand', width: width,height: height);
+  
+  //사람이 O그린 아이콘
+  static Widget imojiO({double width = 24.0,double height = 24.0}) => SfacIconWidget('imojiO', width: width,height: height);
+  
+  //사람이 X그린 아이콘
+  static Widget imojiX({double width = 24.0,double height = 24.0}) => SfacIconWidget('imojiX', width: width,height: height);
+  
+  //노트북 아이콘
+  static Widget laptop({double width = 24.0,double height = 24.0}) => SfacIconWidget('laptop', width: width,height: height);
+  
+  //자물쇠 아이콘
+  static Widget lock({double width = 24.0,double height = 24.0}) => SfacIconWidget('lock', width: width,height: height);
+  
+  //팔레트 아이콘
+  static Widget palette({double width = 24.0,double height = 24.0}) => SfacIconWidget('palette', width: width,height: height);
+  
+  //폰 아이콘
+  static Widget phone({double width = 24.0,double height = 24.0}) => SfacIconWidget('phone', width: width,height: height);
+  
+  //되감기 아이콘
+  static Widget rewind({double width = 24.0,double height = 24.0}) => SfacIconWidget('rewind', width: width,height: height);
+  
+  //로봇머리 아이콘
+  static Widget robot({double width = 24.0,double height = 24.0}) => SfacIconWidget('robot', width: width,height: height);
+  
+  //스피커 아이콘
+  static Widget speaker({double width = 24.0,double height = 24.0}) => SfacIconWidget('speaker', width: width,height: height);
+}
--- lib/widget/badge.dart
+++ lib/widget/badge.dart
@@ -3,19 +3,19 @@ import 'package:widgets/util/sfac_color.dart';
 import 'package:widgets/util/sfac_text_style.dart';
 
 class SfacBadge extends StatelessWidget {
-  const SfacBadge(
-      {super.key,
-      required this.keyWord,
-      required this.keyWordColor,
-      this.backgroundColor= SfacColor.primary80,
-      this.outlineColor,
-      this.borderRadius = 4,
-      this.width = 55,
-      this.height = 20,
-      });
+  const SfacBadge({
+    super.key,
+    required this.keyWord,
+    this.keyWordColor,
+    this.backgroundColor,
+    this.outlineColor,
+    this.borderRadius = 4,
+    this.width = 55,
+    this.height = 20,
+  });
 
   final String keyWord;
-  final Color keyWordColor;
+  final Color? keyWordColor;
   final Color? backgroundColor;
   final Color? outlineColor;
   final double borderRadius;
@@ -28,13 +28,13 @@ class SfacBadge extends StatelessWidget {
       width: width,
       height: height,
       decoration: BoxDecoration(
-        color: backgroundColor,
-        border: outlineColor != null ? Border.all(color: outlineColor!) : null,
-        borderRadius: BorderRadius.circular(borderRadius)
-      ),
+          color: backgroundColor ?? SfacColor.grayScale5,
+          border: Border.all(color: outlineColor ?? SfacColor.grayScale5),
+          borderRadius: BorderRadius.circular(borderRadius)),
       child: Center(
-        child: Text(keyWord, style: SfacTextStyle.b5R12(color: keyWordColor))
-        ),
+          child: Text(keyWord,
+              style: SfacTextStyle.b5R12(
+                  color: keyWordColor ?? SfacColor.grayScale60))),
     );
   }
 }
