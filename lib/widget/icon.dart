d74990f1fe235c5e7512dec4af05abae81fb79e9
marco <tlstmdgh8620@gmail.com>
2023-07-03 16:16:21 +0900

    icon, textarea , 폰트

--- lib/widget/icon.dart
+++ lib/widget/icon.dart
@@ -1,25 +1,21 @@
 import 'package:flutter/material.dart';
 
-class SfacIconWidget extends StatelessWidget {
-  const SfacIconWidget(
-      this.iconName,
-    {
-      super.key,
-    this.width = 24.0,
-    this.height = 24.0
-    });
-    
-  final String iconName;
-  final double width;
-  final double height;
+class SFIcon extends StatelessWidget {
+  const SFIcon(
+  this.iconName,
+    {super.key,
+  this.size
+  });
 
+  final String? iconName;
+  final double? size;
 
   @override
   Widget build(BuildContext context) {
     return Image.asset(
       'assets/icons/$iconName.png',
-      width: width,
-      height: height,
+      width: size,
+      height: size,
       fit: BoxFit.cover,
     );
   }
